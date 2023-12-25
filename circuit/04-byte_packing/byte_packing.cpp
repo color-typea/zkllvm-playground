1509dac89026251dac1264892618f4124d9da457
color-typea <colorcode6a@gmail.com>
2023-12-25 13:05:15 +0800

    Working version of byte packing (change endianness via arithmetics, put into field via bit (de)composition)

--- circuit/04-byte_packing/byte_packing.cpp
+++ circuit/04-byte_packing/byte_packing.cpp
@@ -15,18 +15,41 @@ bool is_same(block_type block0, block_type block1){
     return block0[0] == block1[0] && block0[1] == block1[1];
 }
 
-block_type pack_4xuint64_into_block_type(uint64_t e1, uint64_t e2, uint64_t e3, uint64_t e4) {
-    std::array<typename field_type::value_type, 128> decomposed_block_1;
-    std::array<typename field_type::value_type, 128> decomposed_block_2;
+// Wish it oculd be an opaque type, but at least for documentation purposes
+using uint64_t_le = uint64_t;
+using uint64_t_be = uint64_t;
 
-    __builtin_assigner_bit_decomposition(decomposed_block_1.data()     , 64, e1, BYTE_ORDER_LSB);
-    __builtin_assigner_bit_decomposition(decomposed_block_1.data() + 64, 64, e2, BYTE_ORDER_LSB);
-    __builtin_assigner_bit_decomposition(decomposed_block_2.data()     , 64, e3, BYTE_ORDER_LSB);
-    __builtin_assigner_bit_decomposition(decomposed_block_2.data() + 64, 64, e4, BYTE_ORDER_LSB);
+uint64_t changeEndianness(uint64_t val) {
+    uint64_t result;
+    result += ((val >> (0 * 8)) - ((val >> (1 * 8)) << 8)) << (7 * 8);
+    result += ((val >> (1 * 8)) - ((val >> (2 * 8)) << 8)) << (6 * 8);
+    result += ((val >> (2 * 8)) - ((val >> (3 * 8)) << 8)) << (5 * 8);
+    result += ((val >> (3 * 8)) - ((val >> (4 * 8)) << 8)) << (4 * 8);
+    result += ((val >> (4 * 8)) - ((val >> (5 * 8)) << 8)) << (3 * 8);
+    result += ((val >> (5 * 8)) - ((val >> (6 * 8)) << 8)) << (2 * 8);
+    result += ((val >> (6 * 8)) - ((val >> (7 * 8)) << 8)) << (1 * 8);
+    result += ((val >> (7 * 8))                          ) << (0 * 8);
+    return result;
+}
 
-    return  {
-        __builtin_assigner_bit_composition(decomposed_block_1.data(), 128, BYTE_ORDER_LSB), 
-        __builtin_assigner_bit_composition(decomposed_block_2.data(), 128, BYTE_ORDER_LSB)
+uint64_t_be toBigEndian(uint64_t_le val) {
+    return changeEndianness(val);
+}
+uint64_t_le toLittleEndian(uint64_t_be val) {
+    return changeEndianness(val);
+}
+
+field_type::value_type toSha256Field(uint64_t_le lower, uint64_t_le higher) {
+    std::array<field_type::value_type, 128> decomposed_block;
+    __builtin_assigner_bit_decomposition(decomposed_block.data()     , 64, higher, BYTE_ORDER_LSB);
+    __builtin_assigner_bit_decomposition(decomposed_block.data() + 64, 64, lower, BYTE_ORDER_LSB);
+    return __builtin_assigner_bit_composition(decomposed_block.data(), 128, BYTE_ORDER_LSB);
+}
+
+block_type pack_four(uint64_t_be val1, uint64_t_be val2, uint64_t_be val3, uint64_t_be val4) {
+    return {
+        toSha256Field(toLittleEndian(val1), toLittleEndian(val2)),
+        toSha256Field(toLittleEndian(val3), toLittleEndian(val4))
     };
 }
 
@@ -37,6 +60,6 @@ block_type pack_4xuint64_into_block_type(uint64_t e1, uint64_t e2, uint64_t e3, 
     [[private_input]] uint64_t d,
     block_type expected_block
 ) {
-    block_type packed = pack_4xuint64_into_block_type(a, b, c, d);
+    block_type packed = pack_four(a, b, c, d);
     __builtin_assigner_exit_check(is_same(packed, expected_block));
 }

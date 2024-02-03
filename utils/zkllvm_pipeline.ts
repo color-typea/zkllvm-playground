e9cf442580aa5d5e08cd794dd6e272aebe2f1a04
color-typea <colorcode6a@gmail.com>
2024-02-03 12:06:41 +0800

    Update dependencies and proof-generator (renamed executables)

--- utils/zkllvm_pipeline.ts
+++ utils/zkllvm_pipeline.ts
@@ -6,12 +6,14 @@ const PROJECT_DIR = path.dirname(CUR_DIR);
 const ZKLLVM=path.join(PROJECT_DIR, "../zkllvm");
 const PROOF_GENERATOR_FOLDER=path.join(PROJECT_DIR, "../proof-producer");
 
+const PROOF_GENERATOR_VERSION = 'proof-generator-single-threaded';
+// const PROOF_GENERATOR_VERSION = 'proof-generator-multi-threaded';
 
 const COMPILER = path.join(ZKLLVM, "build/libs/circifier/llvm/bin/clang-16");
 const LINKER = path.join(ZKLLVM, "build/libs/circifier/llvm/bin/llvm-link");
 const ASSIGNER = path.join(ZKLLVM, "build/bin/assigner/assigner");
 const TRANSPILER = path.join(ZKLLVM, "build/bin/transpiler/transpiler");
-const PROOF_GENERATOR = path.join(PROOF_GENERATOR_FOLDER, '/build/bin/proof-generator', 'proof-generator');
+const PROOF_GENERATOR = path.join(PROOF_GENERATOR_FOLDER, '/build/bin/', PROOF_GENERATOR_VERSION, PROOF_GENERATOR_VERSION);
 
 export {
     ZKLLVM, COMPILER, LINKER, ASSIGNER, TRANSPILER, PROOF_GENERATOR
